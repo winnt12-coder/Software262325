b1088dad6c8e2ddc3c0f05a427cf510b686e9c8e
winnt12-coder <winnt12@kookmin.ac.kr>
2026-03-25 13:35:41 +0900

    백준 10996

---
+++ 10996.py
@@ -0,0 +1,10 @@
+N = int(input())
+
+if(N%2 == 0):
+    for i in range(1, N+1):
+        print('* '*(N//2))
+        print(' *'*(N//2))
+elif(N%2 != 0):
+    for j in range(1, N+1):
+        print('* '*(N//2+1))
+        print(' *'*(N//2))
